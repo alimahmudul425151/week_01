232dc548f69d6697e8454e5028afbf59ceaad8b1
Ali <mi7710718@gmial.com>
2024-09-11 00:01:39 +0600

    updated

--- syntex.php
+++ syntex.php
@@ -3,16 +3,18 @@
 $studentName="Hasan Ali";// String.
 $studentAge= 27; // integer.
 $studentHeight= 69.50; // float.
- 
+$isStudent="true";
 $students=["Hasan","Ali","Mahmudul"]; //array
 
 
 
 // echo $studentAge;
 // var_dump($students);
-var_dump($students);
- die();
-echo "<pre>";
-print_r($students);
-echo "</pre>";
- echo "$studentAge";
+// var_dump($students);
+//  die();
+// echo "<pre>";
+// print_r($students);
+// echo "</pre>";
+//  echo "$studentAge";
+ 
+ echo $isStudent;
